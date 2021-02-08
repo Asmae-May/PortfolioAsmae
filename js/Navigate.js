cba5944ce2c40b0da036a55eab82b81437b82cea
Asmae-May <asmae12.elmaymouni@gmail.com>
2021-02-08 22:45:13 +0100

    AboutPost

--- js/Navigate.js
+++ js/Navigate.js
@@ -1,3 +1,9 @@
 function NavigerPost() {
     window.location = '../aboutPost.html'; 
 }
+function NavigerProfile(){
+    window.location = '../aboutProfile.html'; 
+}
+function NavigerSkills(){
+    window.location = '../aboutSkills.html'; 
+}
