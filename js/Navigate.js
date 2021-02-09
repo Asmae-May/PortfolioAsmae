7346119d8b11b79e31915943321fc8a730bc0886
Asmae-May <asmae12.elmaymouni@gmail.com>
2021-02-09 17:47:13 +0100

    aboutSkills 1

--- js/Navigate.js
+++ js/Navigate.js
@@ -7,3 +7,18 @@ function NavigerProfile(){
 function NavigerSkills(){
     window.location = '../aboutSkills.html'; 
 }
+function NavigerWork(){
+    window.location = '../aboutWork.html'; 
+}
+function NavigerBackEND(){
+    window.location = '../BackEnd.html'; 
+}
+function NavigerFrontEnd(){
+    window.location = '../BackEnd.html'; 
+}
+function NavigerDesign(){
+    window.location = '../BackEnd.html'; 
+}
+function NavigerDB(){
+    window.location = '../BackEnd.html'; 
+}
