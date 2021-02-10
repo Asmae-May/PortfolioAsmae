f07a7b388d0d6bfbfc99557c4796360fa17b5959
Asmae-May <asmae12.elmaymouni@gmail.com>
2021-02-10 16:04:53 +0100

    backEnd

--- js/Navigate.js
+++ js/Navigate.js
@@ -10,15 +10,3 @@ function NavigerSkills(){
 function NavigerWork(){
     window.location = '../aboutWork.html'; 
 }
-function NavigerBackEND(){
-    window.location = '../BackEnd.html'; 
-}
-function NavigerFrontEnd(){
-    window.location = '../BackEnd.html'; 
-}
-function NavigerDesign(){
-    window.location = '../BackEnd.html'; 
-}
-function NavigerDB(){
-    window.location = '../BackEnd.html'; 
-}
