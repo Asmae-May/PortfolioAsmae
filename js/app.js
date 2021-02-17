ed881a732e2eab09d4a0ef21f8e2e0164dcf4cb9
Asmae-May <asmae12.elmaymouni@gmail.com>
2021-02-17 17:18:10 +0100

    about skills

--- js/app.js
+++ js/app.js
@@ -1,15 +1,37 @@
+/******************** ouvrir une nouvelle page HTML apres le clic  ***************** */
+
 $('.header').load('../common/header.html');
 $('.PicContatc').load('../common/PicContatc.html');
-//$('.BE').load('BackEnd.html');
 $('.BE2').load('BE.html');
-$('.FE').load('FrontEnd.html');
+$('.FrontEnd2').load('FE.html');
+$('.Desg').load('Design.html');
+$('.BD').load('DataBase.html');
 
-/******************** Change Button Color OnClick ***************** */
-let btnBE= document.querySelector('#btnBE');
-btnBE.addEventListener('click',() => btnBE.style.backgroundColor='#F6E3CE');
+/******************** Change Button Color OnClick *****************/
 
-let btnFE= document.querySelector('#btnFE');
-btnFE.addEventListener('click',() => btnFE.style.backgroundColor='#F6E3CE');
+var btnBE= document.querySelector('#btnBE');
+    if(btnBE){
+    btnBE.addEventListener('click',() => btnBE.style.backgroundColor='#D3C9C1');
+}
+var btnFE= document.querySelector('#btnFE');
+if(btnFE){
+    var btnFE= document.querySelector('#btnFE');
+    btnFE.addEventListener('click',() => btnFE.style.backgroundColor='#D3C9C1');
+}
 
+var btnDesg= document.querySelector('#btnDesg');
+if(btnDesg){
+    var btnDesg= document.querySelector('#btnDesg');
+    btnDesg.addEventListener('click',() => btnDesg.style.backgroundColor='#D3C9C1');
+}
+var btnDataB= document.querySelector('#btnDataB');
+if(btnDataB){
+    var btnDataB= document.querySelector('#btnDataB');
+    btnDataB.addEventListener('click',() => btnDataB.style.backgroundColor='#D3C9C1');
+}
+
+ 
+
+/******************** Change Menu Color OnClick ***************** */
 
     
