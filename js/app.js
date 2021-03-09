02ec2c5544b9ba063609f7a89859ca267815495e
Asmae-May <asmae12.elmaymouni@gmail.com>
2021-03-09 15:32:28 +0100

    derniere modif flex

--- js/app.js
+++ js/app.js
@@ -10,23 +10,23 @@ $('.BD').load('DataBase.html');
 
 var btnBE= document.querySelector('#btnBE');
     if(btnBE){
-    btnBE.addEventListener('click',() => btnBE.style.backgroundColor='#D3C9C1');
+    btnBE.addEventListener('click',() => btnBE.style.backgroundColor='#C7C7C7');
 }
 var btnFE= document.querySelector('#btnFE');
 if(btnFE){
     var btnFE= document.querySelector('#btnFE');
-    btnFE.addEventListener('click',() => btnFE.style.backgroundColor='#D3C9C1');
+    btnFE.addEventListener('click',() => btnFE.style.backgroundColor='#C7C7C7');
 }
 
 var btnDesg= document.querySelector('#btnDesg');
 if(btnDesg){
     var btnDesg= document.querySelector('#btnDesg');
-    btnDesg.addEventListener('click',() => btnDesg.style.backgroundColor='#D3C9C1');
+    btnDesg.addEventListener('click',() => btnDesg.style.backgroundColor='#C7C7C7');
 }
 var btnDataB= document.querySelector('#btnDataB');
 if(btnDataB){
     var btnDataB= document.querySelector('#btnDataB');
-    btnDataB.addEventListener('click',() => btnDataB.style.backgroundColor='#D3C9C1');
+    btnDataB.addEventListener('click',() => btnDataB.style.backgroundColor='#C7C7C7');
 }
 
  
